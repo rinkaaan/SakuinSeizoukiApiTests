108fbe59d3f8d9c3f4d813793b83d8e0c8fde528
Lincoln Nguyen <137611486+rinkaaan@users.noreply.github.com>
2023-12-18 22:55:37 -0800

    Update pdf_path

--- tests/project/test_new_project.py
+++ tests/project/test_new_project.py
@@ -1,4 +1,5 @@
 import json
+import os
 
 from openapi_client import OpenPdfIn
 
@@ -8,8 +9,10 @@ from tests.clients import projectApi
 class TestNewProject:
 
     def test_open_pdf(self):
+        home_path = os.path.expanduser("~")
+        pdf_path = os.path.join(home_path, "Desktop", "test.pdf")
         params = OpenPdfIn(
-            pdf_path="/Users/lincolnnguyen/Desktop/test.pdf"
+            pdf_path=pdf_path,
         )
         res = projectApi.project_new_pdf_post(params)
         json = res.json()
